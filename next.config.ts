4b31d6dfb464fa490c12adef177339da86a1de8b
DeveloperShariful <sharifulislam78009@gmail.com>
2025-12-06 16:14:34 +0600

    update next.js latest vertion

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   
-  // TypeScript error থাকলে বিল্ড process থামিয়ে দেবে না।
-  // প্রোডাকশনে যাওয়ার আগে এটি false করে দেওয়া বা মুছে ফেলা উচিত।
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  // ESLint error থাকলে বিল্ড process থামিয়ে দেবে না।
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // Next.js-এর ইমেজ অপ্টিমাইজেশনের জন্য অনুমোদিত ডোমেইনগুলো
+  
   images: {
     remotePatterns: [
       {
