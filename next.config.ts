36ea06585c08813c2b3bb9b5dc4d1567a9b0947a
DeveloperShariful <sharifulislam78009@gmail.com>
2026-01-13 14:25:39 +0600

    performance boost

--- next.config.ts
+++ next.config.ts
@@ -5,43 +5,27 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sharifulbuilds.com',
-        pathname: '/wp-content/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gobike.au',
-        pathname: '/wp-content/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gobikes.au',
-        pathname: '/wp-content/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        pathname: '/**', // <-- समाधान: pathname যোগ করা হয়েছে
-      },
-      {
-        protocol: 'https',
-        hostname: 'x.klarnacdn.net',
-        pathname: '/**', // Klarna-এর লোগোর জন্য
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.afterpay.com',
-        pathname: '/**', // Afterpay-এর লোগোর জন্য
-      },
+  experimental: {
+    optimizePackageImports: [
+      'react-icons',
+      'react-select',
+      'framer-motion',
+      'lodash',
+      'date-fns',
+      '@headlessui/react'
     ],
   },
   
-  // দ্রষ্টব্য: Partytown ("worker" strategy)-এর জন্য এখানে আর কোনো কনফিগারেশনের প্রয়োজন নেই।
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'sharifulbuilds.com', pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'gobike.au', pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'gobikes.au', pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'i.ytimg.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'x.klarnacdn.net', pathname: '/**' },
+      { protocol: 'https', hostname: 'static.afterpay.com', pathname: '/**' },
+    ],
+  },
 };
 
-// --- সমাধান: ES Module সিনট্যাক্স ব্যবহার করা হচ্ছে ---
 export default nextConfig;
